24c9e773e69c09c931c10792e4301372bb3ed8cb
DEEPAK NAYAK <>
2020-05-23 18:46:53 +0530

    tests

--- react-exercises/simple-plot-react/src/test/chart.test.js
+++ react-exercises/simple-plot-react/src/test/chart.test.js
@@ -2,8 +2,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
 import renderer from 'react-test-renderer';
+import { generateLineGraphData ,generateBarGraphData} from '../utils'
 
 it('renders without crashing', () => {
   const tree = renderer.create(<App />).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+it('should test if data is available for chart', () => {
+  const data = generateBarGraphData();
+  console.log('data:::', data);
+  expect(data.length).toBeGreaterThan(0);
+});
+
